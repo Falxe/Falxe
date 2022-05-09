2ad6f4f74fad93c425f06842bb1de28da0ae69d6
Falxe <105025964+Falxe@users.noreply.github.com>
2022-05-09 18:00:53 -0500

    Update Falxe.lua

--- Falxe.lua
+++ Falxe.lua
@@ -712,6 +712,54 @@ function MM2()
     end)
 
 
+    
+    PlayerSection:NewButton('Xray [L]', 'Gives you xray (see through walls).', function()
+        local xrayHotkey = Enum.KeyCode.L
+
+        local uis = game:GetService("UserInputService")
+
+
+        local xrayOn = false
+
+
+        uis.InputBegan:Connect(function(inp, processed)
+	
+	
+	        if processed then return end
+
+
+	        if inp.KeyCode == xrayHotkey then
+            
+            
+	        	xrayOn = not xrayOn
+            
+            
+	        	for i, descendant in pairs(workspace:GetDescendants()) do
+                
+	        		if descendant:IsA("BasePart") then
+                    
+	        			if xrayOn then
+                        
+	        				if not descendant:FindFirstChild("OriginalTransparency") then
+                            
+	        					local originalTransparency = Instance.new("NumberValue")
+	        					originalTransparency.Name = "OriginalTransparency"
+	        					originalTransparency.Value = descendant.Transparency
+	        					originalTransparency.Parent = descendant
+	        				end
+                        
+	        				descendant.Transparency = 0.5
+                        
+	        			else
+	        				descendant.Transparency = descendant.OriginalTransparency.Value
+	        			end
+	        		end
+	        	end
+	        end
+        end)
+    end)
+
+
 
 
 
